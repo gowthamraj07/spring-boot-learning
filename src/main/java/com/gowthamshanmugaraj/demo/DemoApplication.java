06e0fe80dc3726f44f1fea46f9d6ad705afc0d82
Gowtham Shanmugaraj <gowthamraj07@gmail.com>
2021-02-14 00:11:25 +0100

    Enable auto configuration
    Why: So that spring boot checks for the available Controllers

--- src/main/java/com/gowthamshanmugaraj/demo/DemoApplication.java
+++ src/main/java/com/gowthamshanmugaraj/demo/DemoApplication.java
@@ -1,9 +1,13 @@
 package com.gowthamshanmugaraj.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class DemoApplication {
 
 	public static void main(String[] args) {
